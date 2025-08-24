4c8e6f4200195ed886be326578934721e1436960
Nikolyamba <niko7tkc@yandex.ru>
2025-08-24 21:25:32 +0300

    i fix exit button

--- windows/main_menu_window.py
+++ windows/main_menu_window.py
@@ -1,10 +1,14 @@
 import arcade
-from arcade.gui import UIGridLayout, UIAnchorLayout, UIFlatButton, UIView
+from arcade.gui import UIGridLayout, UIAnchorLayout, UIFlatButton, UIView, UIManager
+
 
 class MainMenu(UIView):
     def __init__(self):
         super().__init__()
         self.background_color = arcade.color.PINK
+
+        self.manager = UIManager()
+        self.manager.enable()
 
         grid = UIGridLayout(
             column_count=3,
@@ -14,7 +18,7 @@ class MainMenu(UIView):
             horizontal_spacing=10,
         )
 
-        self.ui.add(UIAnchorLayout(children=[grid]))
+        self.manager.add(UIAnchorLayout(children=[grid]))
 
         exit_button = UIFlatButton(text="Выйти из игры", width=200)
         start_button = UIFlatButton(text="Начать игру", width=200)
@@ -24,6 +28,10 @@ class MainMenu(UIView):
         grid.add(UIFlatButton(text="Сохранить игру", width=200), row=2, column=0)
         grid.add(exit_button, row=3, column=0)
 
-        @exit_button.event
+        @exit_button.event("on_click")
         def on_click_exit(event):
             arcade.exit()
+
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
